ab4377cf03bcf2dc1d6cc27b0b0c193ae1ec9234
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-09 12:47:19 -0800

    Working

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,3 +1,4 @@
+
 import '../../stylesheets/ColorList.scss'
 import PropTypes from 'prop-types'
 import Color from './Color'
@@ -7,23 +8,15 @@ import { sortFunction } from '../lib/array-helpers'
 const ColorList = ({ store }) => {
     const { colors, sort } = store.getState()
     const sortedColors = [...colors].sort(sortFunction(sort))
+    
     return (
         <div className="color-list">
             {(colors.length === 0) ?
                 <p>No Colors Listed. (Add a Color)</p> :
                 sortedColors.map(color =>
-                    <Color key={color.id}
-                        {...color}
-                           onRate={(rating) =>
-                               store.dispatch(
-                                   rateColor(color.id, rating)
-                               )
-                       }
-                           onRemove={() =>
-                               store.dispatch(
-                                   removeColor(color.id)
-                               )
-                       }
+                    <Color key={color.id}  {...color}
+                        onRate={(rating) => store.dispatch(rateColor(color.id, rating))}
+                        onRemove={() => store.dispatch(removeColor(color.id))}
                     />
                 )
             }
--- src/lib/time-helpers.js
+++ src/lib/time-helpers.js
@@ -1,15 +1,15 @@
 
-const second = 1000,
-    minute = 60 * second,
-    hour = 60 * minute,
-    day = 24 * hour,
-    timeframe = { second, minute, hour, day },
-    breakpoints = {
-        second: 60,
-        minute: 60,
-        hour: 24,
-        day: 30
-    }
+const second = 1000
+const minute = 60 * second
+const hour = 60 * minute
+const day = 24 * hour
+const timeframe = { second, minute, hour, day }
+const breakpoints = {
+    second: 60,
+    minute: 60,
+    hour: 24,
+    day: 30
+}
 
 const toDate = timeStampString => new Date(timeStampString)
 
@@ -19,16 +19,14 @@ const isUnderTime = (diff, timeframe, time) => diff / timeframe < time
 
 const diffOverTimeframe = (diff, timeframe) => Math.floor(diff / timeframe)
 
-const printResult = (result, timeframeName) =>
-    `${result} ${timeframeName + ((result > 1) ? "s" : "")}`
+const printResult = (result, timeframeName) => `${result} ${timeframeName + ((result > 1) ? "s" : "")}`
 
 const checkDate = (diff, timeframeName, underTime, timeframe) =>
     (isUnderTime(diff, timeframe[timeframeName], underTime)) ?
         printResult(diffOverTimeframe(diff, timeframe[timeframeName]), timeframeName) :
         null
 
-const printFullDate = dateTime =>
-    `${dateTime.getMonth() + 1}/${dateTime.getDate()}/${dateTime.getFullYear()}`
+const printFullDate = dateTime => `${dateTime.getMonth() + 1}/${dateTime.getDate()}/${dateTime.getFullYear()}`
 
 const lessThanAMinute = timeString =>
     (timeString.match(/seconds/)) ?
@@ -51,5 +49,4 @@ const howLongAgo = (remainingTimeframe, timeframe, timestamp, now) =>
         printFullDate(toDate(timestamp)) :
         checkNext(remainingTimeframe, timeframe, timestamp, now)
 
-export const ago = (timestamp, now = new Date().toString()) =>
-    howLongAgo(Object.keys(timeframe), timeframe, timestamp, now)
+export const ago = (timestamp, now = new Date().toString()) => howLongAgo(Object.keys(timeframe), timeframe, timestamp, now)
